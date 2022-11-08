365bcde11ea12aa2e4d145a63ab2929e755c5673
quyet <code.maito@gmail.com>
2022-11-08 21:15:44 +0700

    remove dev requirement when done deploy server

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,4 @@
-sudo /py/bin/pip install -r /requirements/requirements.dev.txt
 coverage run manage.py test
 coverage report
 flake8 --exclude=core/migrations/
-sudo /py/bin/pip uninstall -r /requirements/requirements.dev.txt
+/py/bin/pip uninstall -r /requirements/requirements.dev.txt
