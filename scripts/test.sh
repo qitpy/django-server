d45467a33efbe599521d34c4cfb2ce645423d2fc
quyet <code.maito@gmail.com>
2022-11-08 21:10:21 +0700

    add sudo

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
-/py/bin/pip install -r /requirements/requirements.dev.txt
+sudo /py/bin/pip install -r /requirements/requirements.dev.txt
 coverage run manage.py test
 coverage report
 flake8 --exclude=core/migrations/
-/py/bin/pip uninstall -r /requirements/requirements.dev.txt
+sudo /py/bin/pip uninstall -r /requirements/requirements.dev.txt
