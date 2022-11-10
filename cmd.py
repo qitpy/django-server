09f827ce163ddfc2c696e8bf1e139de72e1d7387
quyet <code.maito@gmail.com>
2022-11-10 22:12:44 +0700

    update Dev cli

--- cmd.py
+++ cmd.py
@@ -1,8 +1,18 @@
 import subprocess
 import shlex
+import datetime
 
-commands = [
-
+'''
+    type your command here:
+    example: ('command_name': 'command')
+'''
+COMMANDS = [
+    ('BUILD', 'docker-compose build'),
+    ('RUN', 'docker-compose up'),
+    ('DOWN', 'docker-compose down'),
+    ('TEST', 'docker-compose run --rm src sh -c "python manage.py test"'),
+    ('COVERAGE', 'docker-compose run --rm src sh -c "coverage run manage.py test && coverage report"'),
+    ('FLAKE8', 'docker-compose run --rm src sh -c flake8 --exclude=core/migrations/'),
 ]
 
 
@@ -15,19 +25,14 @@ P = '\033[35m'  # purple
 
 
 def get_user_input() -> int:
-    print("\n\t\t\t* " + R + "♥" + W + " * * * * * * * * * * * * * * * * * * *")
+    print("\n\t\t\t*" + R + " ♥ " + W + "* * * * * * * * * * * * * * * * * * *")
     print("\t\t\t*   " + G + "Developer" + W + " Command Line Interface:\t*")
-    print('\t\t\t*\t\t\t\t\t*')
-    print('\t\t\t*\t1 - BUILD\t\t\t*')
-    print('\t\t\t*\t2 - RUN\t\t\t\t*')
-    print('\t\t\t*\t3 - DOWN\t\t\t*')
-    print('\t\t\t*\t4 - TEST\t\t\t*')
-    print('\t\t\t*\t5 - COVERAGE\t\t\t*')
-    print('\t\t\t*\t6 - FLAKE8\t\t\t*')
-    print('\t\t\t*\t0 - exit\t\t\t*')
-    print("\t\t\t* * * * * * * * * * * * * * * * * * * " + R + "♥" + W + " *")
-    number = input('\t\t\t your choice: ')
+    print("\t\t\t* * * * * * * * * * * * * * * * * * *" + R + " ♥ " + W + "*")
+    for i in range(0, len(COMMANDS)):
+        print(f'\t\t\t {i} - {COMMANDS[i][0]} {B}{COMMANDS[i][1]}{W}')
+    print(f'\t\t\t {len(COMMANDS)} - exit')
 
+    number = input(f'\t\t\t {P}enter a number: {W}')
     if not number.isdigit() or int(number) < 0 or int(number) > 6:
         print('Inputs must be a numbers (0-6)')
         return -1
@@ -39,40 +44,28 @@ def execute_command(command):
     print()
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
     try:
-        print(G + "===> Executing: " + R + command)
+        print(G + f"{datetime.datetime.now()}> Executing: " + R + command)
         while True:
             output = process.stdout.readline()
             if not output and process.poll() is not None:
-                rc = process.poll()
-                print(G + "===> Done with exit status: " + R + str(rc) + W)
+                exit_status = process.poll()
+                print(G + f"{datetime.datetime.now()}> Done with exit status: " + R + str(exit_status) + W)
                 break
             if output:
                 print(O + output.strip().decode())
     except Exception as e:
         print("Interrupt exception!!: " + e.__str__())
-    return rc
 
 
 def re_call_cli():
     number = -1
     while number == -1:
         number = get_user_input()
-        if number == -1:
+        if 0 < number < len(COMMANDS):
+            execute_command(COMMANDS[number][1])
+        elif number == -1:
             continue
-        elif number == 1:
-            execute_command('docker-compose build')
-        elif number == 2:
-            execute_command('docker-compose up')
-        elif number == 3:
-            execute_command('docker-compose down')
-        elif number == 4:
-            execute_command('docker-compose run --rm src sh -c "python manage.py test"')
-        elif number == 5:
-            execute_command('docker-compose run --rm src sh -c "coverage run manage.py test"')
-            execute_command('docker-compose run --rm src sh -c "coverage report"')
-        elif number == 6:
-            execute_command('docker-compose run --rm src sh -c flake8 --exclude=core/migrations/')
-        elif number == 0:
+        elif number == len(COMMANDS):
             print("\n" + P + "Exiting - Good luck! :))" + W)
             break
         number = -1
